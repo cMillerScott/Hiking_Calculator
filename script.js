6b85113a9f2c8526bc1cafaa118bd81fe84253f5
Christopher <c.millerscott@gmail.com>
2021-07-11 07:35:20 -0500

    working on elevation gain

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 //references to HTML user input elements
-let totalMiles = document.getElementById("totalMiles");
-let milesPerHour = document.getElementById("milesPerHour");
-let hoursPerDay = document.getElementById("hoursPerDay");
-let totalElevationGain = document.getElementById("totalElevationGain");
-let totalDays = document.getElementById("totalDays");
+const totalMiles = document.getElementById("totalMiles");
+const milesPerHour = document.getElementById("milesPerHour");
+const hoursPerDay = document.getElementById("hoursPerDay");
+const totalElevationGain = document.getElementById("totalElevationGain");
+const totalDays = document.getElementById("totalDays");
 let totalHours = 0;
+
+//takes user imput value of totalElevationGain adds it to totalHours
+function addElevationGain() {
+  let elevationHours = ((totalElevationGain.value / 1000) * .5 );
+  totalHours += elevationHours;
+  return totalHours.value;
+}
 
 //takes user input values of totalMiles, milesPerHour, totalElevationGain, and hoursPerDay to return value of totalDays rounded to 2 decimal places
 function getTotalDays() { 
@@ -12,10 +19,6 @@ function getTotalDays() {
   totalDays.value = totalHours / hoursPerDay.value;
   totalDays.value = Math.round(totalDays.value * 100) / 100;
   return totalDays.value;
-}
-
-function addElevationGain() {
-  totalHours = ((totalElevationGain.value / 1000) * .5 );
 }
 
 //takes user input values for milesPerHour, hoursPerDay, and totalDays to return value of totalMiles rounded to 2 decimal places
@@ -26,8 +29,8 @@ function getTotalMiles() {
   return totalMiles.value;
 }
 
-totalMiles.addEventListener("keyup", getTotalDays, addElevationGain);
+totalMiles.addEventListener("keyup", getTotalDays);
 milesPerHour.addEventListener("keyup", getTotalDays, getTotalMiles);
 hoursPerDay.addEventListener("keyup", getTotalDays, getTotalMiles);
 totalElevationGain.addEventListener("keyup", getTotalDays, getTotalMiles);
-totalDays.addEventListener("keyup", getTotalMiles, addElevationGain);
+totalDays.addEventListener("keyup", getTotalMiles);
